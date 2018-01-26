7692b6183e691e9b92d1e5f61b8b38976024c2c2
Guilhem Bonnefille <guilhem.bonnefille@c-s.fr>
2018-01-26 18:37:16 +0100

    Add a test to observe PAIR's behaviour when congested

--- src/test/java/zmq/TestPairTcp.java
+++ src/test/java/zmq/TestPairTcp.java
@@ -19,7 +19,16 @@
 
 package zmq;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 import org.junit.Test;
+
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
@@ -51,4 +60,70 @@ public class TestPairTcp
         ZMQ.term(ctx);
 
     }
+
+    @Test
+    public void testCongestionPairTpc() throws InterruptedException
+    {
+        Ctx ctx = ZMQ.init(1);
+        assertThat(ctx, notNullValue());
+        SocketBase sb = ZMQ.socket(ctx, ZMQ.ZMQ_PAIR);
+        assertThat(sb, notNullValue());
+        boolean brc = ZMQ.bind(sb, "tcp://127.0.0.1:6570");
+        assertThat(brc, is(true));
+
+        SocketBase sc = ZMQ.socket(ctx, ZMQ.ZMQ_PAIR);
+        assertThat(sc, notNullValue());
+        brc = ZMQ.connect(sc, "tcp://127.0.0.1:6570");
+        assertThat(brc, is(true));
+
+        sendUntilBlock(sc);
+
+        //  Tear down the wiring.
+        ZMQ.close(sb);
+        ZMQ.close(sc);
+        ZMQ.term(ctx);
+
+    }
+
+   protected void sendUntilBlock(SocketBase sc) throws InterruptedException
+   {
+       byte[] content = "12345678ABCDEFGH12345678abcdefgh".getBytes(ZMQ.CHARSET);
+
+       runUntilBlock(() -> {
+           int rc = ZMQ.send(sc, content, 32, ZMQ.ZMQ_SNDMORE);
+           assertThat(rc, is(32));
+           rc = ZMQ.send(sc, content, 32, 0);
+           assertThat(rc, is(32));
+           return true;
+           }, 10, TimeUnit.SECONDS);
+   }
+
+   protected <V> void runUntilBlock(Callable<V> runnable, long timeout, TimeUnit unit) throws InterruptedException
+   {
+       boolean blocked = false;
+       ExecutorService executor = Executors.newFixedThreadPool(1);
+       while(!blocked)
+       {
+           FutureTask<V> task = new FutureTask<V>(runnable);
+           executor.execute(task);
+           try
+           {
+               task.get(timeout, unit);
+               blocked = false;
+           }
+           catch (InterruptedException e)
+           {
+               throw e;
+           }
+           catch (ExecutionException e)
+           {
+               // Should not occur due to Runnable
+               e.printStackTrace();
+           }
+           catch (TimeoutException e)
+           {
+               blocked = true;
+           }
+       }
+   }
 }
